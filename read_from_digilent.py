58fa8de36861d8c76a6f67b77c0a348bdbecbf36
SolomonKritz <solomonkritz@gmail.com>
2019-03-05 21:55:56 -0500

    Transmitting and Working Digilent SDK

---
+++ read_from_digilent.py
@@ -0,0 +1,140 @@
+"""
+   DWF Python Example
+   Author:  Digilent, Inc.
+   Revision:  2018-07-19
+
+   Requires:                       
+       Python 2.7, 3
+"""
+
+from ctypes import *
+from dwfconstants import *
+import math
+import time
+import matplotlib.pyplot as plt
+import sys
+import numpy
+import serial
+
+if sys.platform.startswith("win"):
+    dwf = cdll.dwf
+elif sys.platform.startswith("darwin"):
+    dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+else:
+    dwf = cdll.LoadLibrary("libdwf.so")
+
+#declare ctype variables
+hdwf = c_int()
+sts = c_byte()
+hzAcq = c_double(100)
+nSamples = 2000
+rgdSamples = (c_double*nSamples)()
+cAvailable = c_int()
+cLost = c_int()
+cCorrupted = c_int()
+fLost = 0
+fCorrupted = 0
+
+ser = serial.Serial('COM8', 19200)
+
+#print(DWF version
+version = create_string_buffer(16)
+dwf.FDwfGetVersion(version)
+print("DWF Version: "+str(version.value))
+
+#open device
+print("Opening first device")
+dwf.FDwfDeviceOpen(c_int(-1), byref(hdwf))
+
+if hdwf.value == hdwfNone.value:
+    szerr = create_string_buffer(512)
+    dwf.FDwfGetLastErrorMsg(szerr)
+    print(str(szerr.value))
+    print("failed to open device")
+    quit()
+
+# print("Generating sine wave...")
+# dwf.FDwfAnalogOutNodeEnableSet(hdwf, c_int(0), AnalogOutNodeCarrier, c_bool(True))
+# dwf.FDwfAnalogOutNodeFunctionSet(hdwf, c_int(0), AnalogOutNodeCarrier, funcSine)
+# dwf.FDwfAnalogOutNodeFrequencySet(hdwf, c_int(0), AnalogOutNodeCarrier, c_double(1))
+# dwf.FDwfAnalogOutNodeAmplitudeSet(hdwf, c_int(0), AnalogOutNodeCarrier, c_double(2))
+# dwf.FDwfAnalogOutConfigure(hdwf, c_int(0), c_bool(True))
+
+#set up acquisition
+dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(0), c_bool(True))
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(0), c_double(5))
+dwf.FDwfAnalogInAcquisitionModeSet(hdwf, c_int(1)) #acqmodeScanShift
+dwf.FDwfAnalogInFrequencySet(hdwf, hzAcq)
+dwf.FDwfAnalogInRecordLengthSet(hdwf, -1) # -1 infinite record length
+
+#wait at least 2 seconds for the offset to stabilize
+time.sleep(2)
+
+print("Starting oscilloscope")
+dwf.FDwfAnalogInConfigure(hdwf, c_int(0), c_int(1))
+
+cSamples = 0
+cStart = 0;
+
+# plt.axis([0, len(rgdSamples), -6, 6])
+# plt.ion()
+# hl, = plt.plot([], [])
+# hl.set_xdata(range(0, len(rgdSamples)))
+
+
+while True:
+    dwf.FDwfAnalogInStatus(hdwf, c_int(1), byref(sts))
+    if cSamples == 0 and (sts == DwfStateConfig or sts == DwfStatePrefill or sts == DwfStateArmed) :
+        # Acquisition not yet started.
+        continue
+
+    dwf.FDwfAnalogInStatusRecord(hdwf, byref(cAvailable), byref(cLost), byref(cCorrupted))
+    
+    cSamples += cLost.value
+
+    if cLost.value :
+        fLost = 1
+        print("LOST\n");
+    if cCorrupted.value :
+        fCorrupted = 1
+        print("CORR\n");
+
+    if cAvailable.value==0 :
+        continue
+
+    if cSamples+cAvailable.value > nSamples :
+        cAvailable = c_int(nSamples-cSamples)
+    dwf.FDwfAnalogInStatusData(hdwf, c_int(0), byref(rgdSamples, sizeof(c_double)*cSamples), cAvailable) # get channel 1 data
+    
+    cSamples += cAvailable.value
+    ser.write((str([ '{0:+.4f}'.format(round(elem, 4)) for elem in rgdSamples[cStart:cSamples] ]).strip('[]').replace(" ", "").replace("'", "")+",").encode("ascii"))
+    
+    cStart += cAvailable.value;
+    
+    # hl.set_ydata(rgdSamples)
+    # plt.draw()
+    # plt.pause(0.01)
+
+    if (cSamples >= nSamples):
+        cSamples = 0;
+        cStart = 0;
+    
+
+dwf.FDwfAnalogOutReset(hdwf, c_int(0))
+dwf.FDwfDeviceCloseAll()
+
+print("Recording done")
+if fLost:
+    print("Samples were lost! Reduce frequency")
+if fCorrupted:
+    print("Samples could be corrupted! Reduce frequency")
+
+f = open("record.csv", "w")
+for v in rgdSamples:
+    f.write("%s\n" % v)
+f.close()
+  
+# plt.plot(numpy.fromiter(rgdSamples, dtype = numpy.float))
+# plt.show()
+
+
